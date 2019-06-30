018c5d5155838f9807fcefd894127e5fa2f27862
JuanT <juaniman.2000@gmail.com>
2019-06-30 18:21:59 +0200

    test: create empty test files
    created for prevent lint-staged error

---
+++ __tests__/controller.test.ts
@@ -0,0 +1,13 @@
+import { Controller } from '../src/controller';
+
+describe('', () => {
+  let controller;
+
+  beforeEach(() => {
+    controller = new Controller();
+  });
+
+  xit('should prepare screen', () => {
+    // expect(Controller).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ __tests__/index.test.ts
@@ -0,0 +1,5 @@
+import { Controller } from '../src/controller';
+
+xit('should call Controller', () => {
+  expect(Controller).toHaveBeenCalledTimes(1);
+});
