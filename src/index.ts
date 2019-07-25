05dec3dbbe13b0ee987204a994389d4b541e8ce1
Nya Garcia <nyablk97@gmail.com>
2019-07-25 12:50:44 +0200

    feat: add stream to observable conversion

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
-import { Controller } from './controller';
+import { ConsoleService } from './services/console.service';
+import { Controller } from './controller2';
+import { FilesService2 } from './services/files2.service';
+import { SpinnerService } from './services/spinner.service';
 
-export const controller = new Controller();
+export const controller = new Controller(
+  new FilesService2(),
+  new SpinnerService(),
+  new ConsoleService(),
+);
--- src/services/files2.service.ts
+++ src/services/files2.service.ts
@@ -1,28 +1,41 @@
 import * as getSize from 'get-folder-size';
 
-import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import { ChildProcessWithoutNullStreams, exec, spawn } from 'child_process';
+import { Observable, of } from 'rxjs';
 
-import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export class FilesService2 {
-  public async getFolderSize(path: string) {
-    /* const command = spawn('du', ['-h', path, '--max-depth', 0, | cut -f 1]) */
+  public folders: Array<String> = [];
+
+  public getFolderSize(path: string): Observable<any> {
     const du = spawn('du', ['-h', path, '--max-depth', '0']);
     const cut = spawn('cut', ['-f', '1']);
 
     du.stdout.pipe(cut.stdin);
+    this.setEncoding(cut);
 
-    for await (const data of cut.stdout) {
-      return data;
-    }
+    return this.convertStream(cut);
   }
 
-  public listDir(path: string) {
+  /*  public getFolderSize(path: string) {
+    return exec(`du -h ${path} --max-depth 0 | cut -f 1`);
+  } */
+
+  public listDir(path: string): Observable<any> {
     const stream = this.getStream(path);
-    stream.stderr.on('error', error => console.log('Process ERROR', error));
-    stream.stdout.on('data', data => console.log(data));
-    stream.stderr.on('data', error => console.error(error));
-    stream.on('end', () => console.log('search completed :)'));
+    /* stream.stderr.on('error', error => console.log('Process ERROR', error));
+    stream.stdout.on('data', data => {
+      const folders = this.splitData(data);
+      folders.map(folder =>
+        this.getFolderSize(folder).stdout.on('data', size =>
+          console.log(folder, size),
+        ),
+      );
+    });
+    stream.stderr.on('data', error => error); */
+
+    return this.convertStream(stream);
   }
 
   private getStream(path: string) {
@@ -38,6 +51,10 @@ export class FilesService2 {
     return child;
   }
 
+  public splitData(data: string) {
+    return data.split('\n');
+  }
+
   public getUserHomePath() {
     return require('os').homedir();
   }
@@ -50,7 +67,48 @@ export class FilesService2 {
     child.stdout.setEncoding('utf8');
     child.stderr.setEncoding('utf8');
   }
+
+  private convertStream(stream: ChildProcessWithoutNullStreams) {
+    const { stdout, stderr } = stream;
+
+    return new Observable(observer => {
+      const dataHandler = data => observer.next(data);
+      const errorHandler = error => observer.error(error);
+      const endHandler = () => observer.complete();
+
+      stdout.addListener('data', dataHandler);
+      stdout.addListener('error', errorHandler);
+      stdout.addListener('end', endHandler);
+
+      stderr.addListener('data', errorHandler);
+      stderr.addListener('error', errorHandler);
+
+      return () => {
+        stdout.removeListener('data', dataHandler);
+        stdout.removeListener('error', errorHandler);
+        stdout.removeListener('end', endHandler);
+
+        stderr.removeListener('data', errorHandler);
+        stderr.removeListener('error', errorHandler);
+      };
+    });
+  }
 }
 
-const fs = new FilesService2();
-fs.listDir('/');
+/* const fs = new FilesService2();
+fs.listDir('/home').subscribe(
+  value => {
+    const folders = fs.splitData(value);
+    folders.map(folder => {
+      fs.getFolderSize(folder).subscribe(size =>
+        console.log('FOLDER', folder, 'SIZE', size),
+      );
+    });
+  },
+  error => {
+    console.log(error);
+  },
+  () => {
+    console.log('Search finished :)');
+  },
+); */
