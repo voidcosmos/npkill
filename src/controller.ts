f89b19ab79be39154c1f74bf648d5a230d5efd1e
JuanT <juaniman.2000@gmail.com>
2019-08-17 17:40:29 +0200

    feat(controller): add exit message

--- src/controller.ts
+++ src/controller.ts
@@ -190,8 +190,8 @@ export class Controller {
     return this.stdout.columns <= MIN_CLI_COLUMNS_SIZE;
   }
 
-  private setRawMode(): void {
-    this.stdin.setRawMode(true);
+  private setRawMode(set = true): void {
+    this.stdin.setRawMode(set);
     process.stdin.resume();
   }
 
@@ -343,6 +343,10 @@ export class Controller {
     this.print(ansiEscapes.cursorHide);
   }
 
+  private showCursor(): void {
+    this.print(ansiEscapes.cursorShow);
+  }
+
   private scan(): void {
     const folders$ = this.fileService.listDir(this.folderRoot);
     folders$.subscribe(
@@ -401,8 +405,16 @@ export class Controller {
   }
 
   private quit(): void {
+    this.setRawMode(false);
     this.clear();
+    this.printExitMessage();
+    this.showCursor();
     process.exit();
+  }
+
+  private printExitMessage(): void {
+    const exitMessage = `Space released: ${this.getStats().spaceReleased}\n`;
+    this.print(exitMessage);
   }
 
   private getCommand(keyName: string): string {
