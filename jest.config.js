11d66116d32cd7b05d2e0d892b91598e320ffe35
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2020-04-05 21:58:57 +0200

    chore(test): stryker configuration (#71)
    * chore(test): stryker configuration
    
    * fix(app): typings instead on any
    
    * Revert "fix(app): typings instead on any"
    
    This reverts commit 4e2bafdfca2d1139d84d280bd4a24f578bc13006.

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
