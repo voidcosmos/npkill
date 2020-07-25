2c12f1893d40de23c71da8326446b9251247ccfe
Nya Garcia <nyablk97@gmail.com>
2020-07-25 15:48:18 +0200

    feat(services): add mac-files.service

---
+++ src/services/mac-files.service.ts
@@ -0,0 +1,21 @@
+import { Observable } from 'rxjs';
+import { spawn } from 'child_process';
+import { StreamService } from './stream.service';
+import { map } from 'rxjs/operators';
+import { UnixFilesService } from './unix-files.service';
+
+export class MacFilesService extends UnixFilesService {
+  constructor(streamService: StreamService) {
+    super(streamService);
+  }
+
+  getFolderSize(path: string): Observable<any> {
+    const du = spawn('du', ['-sk', path]);
+    /* stat -f %z | awk '{t+=$1}END{print t}' */
+    const cut = spawn('cut', ['-f', '1']);
+
+    du.stdout.pipe(cut.stdin);
+
+    return this.streamService.getStream(cut).pipe(map((size) => +size));
+  }
+}
