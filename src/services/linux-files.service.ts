174ff95b5b1ec56edffae5328113ed5a1c87764a
Nya Garcia <nyablk97@gmail.com>
2020-07-25 15:49:11 +0200

    fix(linux-files.service): change du parameters in getFolderSize()

--- src/services/linux-files.service.ts
+++ src/services/linux-files.service.ts
@@ -1,67 +1,21 @@
-import { FileService, StreamService } from '@core/services';
-import { exec, spawn } from 'child_process';
+import { StreamService } from '@core/services';
+import { spawn } from 'child_process';
 
-import { IListDirParams } from '@core/interfaces/list-dir-params.interface';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { UnixFilesService } from './unix-files.service';
 
-export class LinuxFilesService extends FileService {
-  constructor(private streamService: StreamService) {
-    super();
+export class LinuxFilesService extends UnixFilesService {
+  constructor(streamService: StreamService) {
+    super(streamService);
   }
 
   getFolderSize(path: string): Observable<{}> {
-    const du = spawn('du', ['-sk', path]);
+    const du = spawn('du', ['-s', '--apparent-size', '-k', path]);
     const cut = spawn('cut', ['-f', '1']);
 
     du.stdout.pipe(cut.stdin);
 
-    return this.streamService
-      .getStream(cut)
-      .pipe(map(size => +size));
-  }
-
-  listDir(params: IListDirParams): Observable<Buffer> {
-    const args = this.prepareFindArgs(params);
-
-    const child = spawn('find', args);
-
-    return this.streamService.getStream(child);
-  }
-
-  deleteDir(path: string): Promise<{}> {
-    return new Promise((resolve, reject) => {
-      const command = `rm -rf "${path}"`;
-      exec(command, (error, stdout, stderr) => {
-        if (error) return reject(error);
-        if (stderr) return reject(stderr);
-        resolve(stdout);
-      });
-    });
-  }
-
-  private prepareFindArgs(params: IListDirParams): string[] {
-    const { path, target, exclude } = params;
-    let args: string[] = [path];
-
-    if (exclude) {
-      args = [...args, this.prepareExcludeArgs(exclude)].flat();
-    }
-
-    args = [...args, '-name', target, '-type', 'd', '-prune'];
-
-    return args;
-  }
-
-  private prepareExcludeArgs(exclude: string[]): string[] {
-    const excludeDirs = exclude.map((dir: string) => [
-      '-not',
-      '(',
-      '-name',
-      dir,
-      '-prune',
-      ')',
-    ]);
-    return excludeDirs.flat();
+    return this.streamService.getStream(cut).pipe(map((size) => +size));
   }
 }
