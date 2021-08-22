c227529ef7c726f4809997ad247235b195d7eb7c
nyagarcia <nyablk97@gmail.com>
2021-08-22 18:30:29 +0200

    refactor(recursive-rmdir-node-support.constants): string to object

--- src/constants/recursive-rmdir-node-support.constants.ts
+++ src/constants/recursive-rmdir-node-support.constants.ts
@@ -1,5 +1,5 @@
-export const RECURSIVE_RMDIR_NODE_VERSION_SUPPORT = '12.10';
-export const RM_NODE_VERSION_SUPPORT = '14.14';
+export const RECURSIVE_RMDIR_NODE_VERSION_SUPPORT = { major: 12, minor: 10 };
+export const RM_NODE_VERSION_SUPPORT = { major: 14, minor: 14 };
 
 export const RECURSIVE_RMDIR_IGNORED_ERROR_CODES: string[] = [
   'ENOTEMPTY',
