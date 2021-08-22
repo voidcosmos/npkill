5dda8e412c70e4a6e2aaf3f1d9a47016f45677cd
nyagarcia <nyablk97@gmail.com>
2021-08-22 16:59:49 +0200

    fix(windows-files.service): instantiate windows strategy manager

--- src/services/windows-files.service.ts
+++ src/services/windows-files.service.ts
@@ -9,11 +9,14 @@ import { normalize } from 'path';
 import { spawn } from 'child_process';
 
 export class WindowsFilesService extends FileService {
+
+  private windowsStrategyManager: WindowsStrategyManager;
+
   constructor(
     private streamService: StreamService,
-    private windowsStrategyManager: WindowsStrategyManager,
   ) {
     super();
+    this.windowsStrategyManager = new WindowsStrategyManager();
   }
 
   getFolderSize(path: string): Observable<number> {
@@ -44,123 +47,4 @@ export class WindowsFilesService extends FileService {
     return this.windowsStrategyManager.deleteDir(path);
   }
 
-  /*    deleteDir(path: string): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      const { major: supportedMajor, minor: supportedMinor } = RECURSIVE_RMDIR_NODE_VERSION_SUPPORT;
-
-      if (this.nodeVersion instanceof Error) {
-        return reject(this.nodeVersion);
-      }
-
-      const { major: currentMajor, minor: currentMinor } = this.nodeVersion;
-
-      if (currentMajor < supportedMajor || (currentMajor === supportedMajor && currentMinor < supportedMinor)) {
-        this.removeRecursively(path, err => {
-          if (err) {
-            return reject(err);
-          }
-          resolve(true);
-        });
-      } else {
-        rm(path, { recursive: true }, err => {
-          if (err) {
-            reject(err);
-          }
-          resolve(true);
-        });
-      }
-    });
-  } */
-
-  /* protected get nodeVersion(): INodeVersion | Error {
-    const releaseVersionsRegExp: RegExp = /^v(\d{1,2})\.(\d{1,2})\.(\d{1,2})/;
-    const versionMatch = version.match(releaseVersionsRegExp);
-
-    if (!versionMatch) {
-      return new Error(`Unable to parse Node version: ${version}`);
-    }
-
-    return {
-      major: parseInt(versionMatch[1], 10),
-      minor: parseInt(versionMatch[2], 10),
-      patch: parseInt(versionMatch[3], 10),
-    };
-  }
-
-  protected removeRecursively(dirOrFilePath: string, callback: IErrorCallback): void {
-    lstat(dirOrFilePath, (lstatError, stats) => {
-      //  No such file or directory - Done
-      if (lstatError && lstatError.code === 'ENOENT') {
-        return callback(null);
-      }
-
-      if(stats.isDirectory()) {
-        return this.removeDirectory(dirOrFilePath, callback);
-      }
-
-      unlink(dirOrFilePath, rmError => {
-        //  No such file or directory - Done
-        if (rmError && rmError.code === 'ENOENT') {
-          return callback(null);
-        }
-
-        if (rmError && rmError.code === 'EISDIR') {
-          return this.removeDirectory(dirOrFilePath, callback);
-        }
-
-        callback(rmError);
-      });
-    });
-  }
-
-  protected removeDirectory(path: string, callback) {
-    rm(path, rmDirError => {
-      //  We ignore certain error codes
-      //  in order to simulate 'recursive' mode
-      if (rmDirError && RECURSIVE_RMDIR_IGNORED_ERROR_CODES.includes(rmDirError.code)) {
-        return this.removeChildren(path, callback);
-      }
-
-      callback(rmDirError);
-    });
-  }
-
-  protected removeChildren(path: string, callback) {
-    readdir(path, (readdirError, ls) => {
-      if (readdirError) {
-        return callback(readdirError);
-      }
-
-      let contentInDirectory = ls.length;
-      let done = false;
-
-      //  removeDirectory only allows deleting directories
-      //  that has no content inside (empty directory).
-      if (!contentInDirectory) {
-        return rm(path, callback);
-      }
-
-      ls.forEach(dirOrFile => {
-        const dirOrFilePath = pathJoin(path, dirOrFile);
-
-        this.removeRecursively(dirOrFilePath, error => {
-          if (done) {
-            return;
-          }
-
-          if (error) {
-            done = true;
-            return callback(error);
-          }
-
-          contentInDirectory--;
-          //  No more content inside.
-          //  Remove the directory.
-          if (!contentInDirectory) {
-            rm(path, callback);
-          }
-        });
-      });
-    });
-  } */
 }
