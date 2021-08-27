b8f8a32e9f9da46e75697a51de3dbecd3438bb77
JuanT <juaniman.2000@gmail.com>
2021-08-27 22:01:23 +0200

    test(files.service): fix bad block close

--- __tests__/files.service.test.ts
+++ __tests__/files.service.test.ts
@@ -87,73 +87,73 @@ describe('File Service', () => {
     fileService.getFileContent(path);
     expect(readFileSyncSpy).toBeCalledWith(path, 'utf8');
   });
-});
 
-describe('Functional test for #deleteDir', () => {
-  let fileService: IFileService;
-  const testFolder = 'test-files';
-  const directories = [
-    'testProject',
-    'awesome-fake-project',
-    'a',
-    'ewez',
-    'potato and bananas',
-  ];
+  describe('Functional test for #deleteDir', () => {
+    let fileService: IFileService;
+    const testFolder = 'test-files';
+    const directories = [
+      'testProject',
+      'awesome-fake-project',
+      'a',
+      'ewez',
+      'potato and bananas',
+    ];
 
-  const createDir = (dir) => mkdirSync(dir);
-  const isDirEmpty = (dir) => readdirSync(dir).length === 0;
-  const createFileWithSize = (filename, mb) =>
-    writeFileSync(filename, Buffer.alloc(1024 * 1024 * mb));
+    const createDir = (dir) => mkdirSync(dir);
+    const isDirEmpty = (dir) => readdirSync(dir).length === 0;
+    const createFileWithSize = (filename, mb) =>
+      writeFileSync(filename, Buffer.alloc(1024 * 1024 * mb));
 
-  beforeAll(() => {
-    const getOS = () => process.platform;
-    const OSService = {
-      linux: LinuxFilesService,
-      win32: WindowsFilesService,
-      darwin: MacFilesService,
-    };
-    const streamService: StreamService = new StreamService();
-    fileService = new OSService[getOS()](streamService);
+    beforeAll(() => {
+      const getOS = () => process.platform;
+      const OSService = {
+        linux: LinuxFilesService,
+        win32: WindowsFilesService,
+        darwin: MacFilesService,
+      };
+      const streamService: StreamService = new StreamService();
+      fileService = new OSService[getOS()](streamService);
 
-    if (existsSync(testFolder)) {
-      rimraf.sync(testFolder);
-    }
-    createDir(testFolder);
+      if (existsSync(testFolder)) {
+        rimraf.sync(testFolder);
+      }
+      createDir(testFolder);
 
-    directories.forEach((dirName) => {
-      const basePath = `${testFolder}/${dirName}`;
-      const targetFolder = `${basePath}/node_modules`;
-      const subfolder = `${targetFolder}/sample subfolder`;
-      createDir(basePath);
-      createDir(targetFolder);
-      createDir(subfolder);
-      createFileWithSize(targetFolder + '/a', 30);
-      createFileWithSize(subfolder + '/sample file', 12);
-      // Create this structure:
-      //   test-files
-      //    |testProject
-      //      |a (file)
-      //      |sample subfolder
-      //       |sample file (file)
-      //    |etc...
+      directories.forEach((dirName) => {
+        const basePath = `${testFolder}/${dirName}`;
+        const targetFolder = `${basePath}/node_modules`;
+        const subfolder = `${targetFolder}/sample subfolder`;
+        createDir(basePath);
+        createDir(targetFolder);
+        createDir(subfolder);
+        createFileWithSize(targetFolder + '/a', 30);
+        createFileWithSize(subfolder + '/sample file', 12);
+        // Create this structure:
+        //   test-files
+        //    |testProject
+        //      |a (file)
+        //      |sample subfolder
+        //       |sample file (file)
+        //    |etc...
+      });
     });
-  });
 
-  afterAll(() => {
-    rimraf.sync(testFolder);
-  });
+    afterAll(() => {
+      rimraf.sync(testFolder);
+    });
 
-  it('Test folder should not be empty', () => {
-    expect(isDirEmpty(testFolder)).toBeFalsy();
-  });
+    it('Test folder should not be empty', () => {
+      expect(isDirEmpty(testFolder)).toBeFalsy();
+    });
 
-  it('Should delete all folders created in test folder', async () => {
-    for (const dirName of directories) {
-      const path = `${testFolder}/${dirName}`;
-      expect(existsSync(path)).toBeTruthy();
-      await fileService.deleteDir(path);
-      expect(existsSync(path)).toBeFalsy();
-    }
-    expect(isDirEmpty(testFolder)).toBeTruthy();
+    it('Should delete all folders created in test folder', async () => {
+      for (const dirName of directories) {
+        const path = `${testFolder}/${dirName}`;
+        expect(existsSync(path)).toBeTruthy();
+        await fileService.deleteDir(path);
+        expect(existsSync(path)).toBeFalsy();
+      }
+      expect(isDirEmpty(testFolder)).toBeTruthy();
+    });
   });
 });
