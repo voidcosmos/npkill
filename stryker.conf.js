82263faf5d1268891ed2c26bb20f7a3e085657c9
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2021-10-30 17:25:06 +0200

    feat(stryker): update to stryker 5.4.1 (#114)

---
+++ stryker.conf.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  packageManager: 'npm',
+  reporters: ['html', 'clear-text', 'progress'],
+  testRunner: 'jest',
+  coverageAnalysis: 'perTest',
+};
