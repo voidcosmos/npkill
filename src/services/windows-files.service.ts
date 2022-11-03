4560ce0b88a26b9a9ae0bc0e7a896d0b1cbffe8c
zaldih <imzaldih@gmail.com>
2022-11-03 16:49:47 +0100

    fix(windows.files.service): implement listDir

--- src/services/windows-files.service.ts
+++ src/services/windows-files.service.ts
@@ -3,16 +3,21 @@ import getSize from 'get-folder-size';
 import { FileService, StreamService } from '../services/index.js';
 
 import { IListDirParams } from '../interfaces/list-dir-params.interface.js';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { WindowsStrategyManager } from '../strategies/windows-remove-dir.strategy.js';
 import { normalize } from 'path';
 import { spawn } from 'child_process';
 import __dirname from '../dirname.js';
+import { runWorker } from './files/files.worker.js';
 
 export class WindowsFilesService extends FileService {
   listDir(params: IListDirParams): Observable<string> {
-    throw new Error('Method not implemented.');
+    const stream$ = new BehaviorSubject(null);
+    runWorker(stream$, params);
+    // this.search(stream, params, params.path);
+    return stream$;
   }
+
   private windowsStrategyManager: WindowsStrategyManager =
     new WindowsStrategyManager();
 
