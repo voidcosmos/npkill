0d388ad963261e6ff0c93134a9a5f143be50dd3b
Juan Torres <imzaldih@gmail.com>
2022-11-07 19:34:18 +0100

    Last workspace usage: show and filter (#134)
    * feat: add days since the project was worked on
    
    -technical proof- Each result is assigned the modification time of the most recent file in the workspace (parent directory of the node_module) and the days that have elapsed will be displayed
    
    * test(results): add missing parameter
    
    * feat(results): sort by date
    
    * fix(sort): fix date sort method
    
    * style: improve columns
    
    * fix: fix sort by date
    
    * perf(sort-date): improve sort by date method
    
    * style: align old column to right
    
    * refactor(last-usage): rename some methods
    
    * fix: handle invalid amount for repeat
    
    * refactor: remove id property
    
    * refactor(header): move columns header to constant
    
    * docs(readme): add last_mod to sort options section
    
    * chore: final adjustaments and describe the feat in readme

--- src/constants/messages.constants.ts
+++ src/constants/messages.constants.ts
@@ -9,7 +9,7 @@ export const INFO_MSGS = {
 
   DISABLED:
     '[-D, --delete-all] option has been disabled until future versions. Please restart npkill without this option.',
-  HEADER_SIZE_COLUMN: 'folder size',
+  HEADER_COLUMNS: 'last_mod   size',
   HELP_TITLE: ' NPKILL HELP ',
   MIN_CLI_CLOMUNS:
     'Oh no! The terminal is too narrow. Please, ' +
@@ -18,7 +18,7 @@ export const INFO_MSGS = {
   NO_TTY:
     // tslint:disable-next-line: max-line-length
     'Oh no! Npkill does not support this terminal (TTY is required). This is a bug, which has to be fixed. Please try another command interpreter (for example, CMD in windows)',
-  NO_VALID_SORT_NAME: 'Invalid sort option. Available: path | size',
+  NO_VALID_SORT_NAME: 'Invalid sort option. Available: path | size | last-mod',
   SEARCHING: 'searching ',
   SEARCH_COMPLETED: 'search completed ',
   SPACE_RELEASED: 'space saved: ',
