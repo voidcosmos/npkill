38cfc8af31860baff09ded24ee2b508a94094ad1
zaldih <imzaldih@gmail.com>
2022-11-09 18:00:30 +0100

    chore(go): say goodbye to files-service.go

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,7 +1,6 @@
 import gulp from 'gulp';
 import ts from 'gulp-typescript';
 import { deleteAsync as del } from 'del';
-import { exec } from 'child_process';
 
 function clean() {
   return del(['./lib']);
@@ -12,53 +11,12 @@ function typescript() {
   return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('lib'));
 }
 
-function copyBin() {
-  const files = ['./src/bin/windows-find.exe'];
-  return gulp.src(files).pipe(gulp.dest('./lib/bin/'));
-}
-
 function copyTsConfig() {
   const files = ['./tsconfig.json'];
   return gulp.src(files).pipe(gulp.dest('./lib'));
 }
 
-async function buildGo() {
-  const env = 'env GOOS=windows GOARCH=amd64';
-  const filePath = './src/libs/file-service.go';
-  const dest = './src/bin/windows-find.exe';
-  // -ldflags "-s -w" reduce the final build
-  const command = `${env} go build -ldflags "-s -w" -o ${dest} ${filePath}`;
-
-  const buildGo = async () =>
-    new Promise((resolve) => {
-      const buildProcess = exec(command, (err) => {
-        if (err) {
-          throw err;
-        }
-      });
-      buildProcess.on('exit', function () {
-        resolve();
-      });
-    });
-
-  const compressExe = async () =>
-    new Promise((resolve) => {
-      const compressProcess = exec(`upx --brute ${dest}`, (err) => {
-        if (err) {
-          throw err;
-        }
-      });
-      compressProcess.on('exit', function () {
-        resolve();
-      });
-    });
-
-  await buildGo();
-  await compressExe();
-}
-
-const buildAll = gulp.series(clean, typescript, copyBin, copyTsConfig);
+const buildAll = gulp.series(clean, typescript, copyTsConfig);
 
 gulp.task('default', buildAll);
-gulp.task('buildGo', buildGo);
 gulp.task('typescript', typescript);
