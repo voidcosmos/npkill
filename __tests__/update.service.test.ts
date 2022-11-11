9633d7181d9b596b4c6fece6dfa7ae6b354b8560
zaldih <imzaldih@gmail.com>
2022-11-11 12:57:19 +0100

    fix(check-update): fix version check when >= 0.10.0

--- __tests__/update.service.test.ts
+++ __tests__/update.service.test.ts
@@ -13,65 +13,98 @@ describe('update Service', () => {
   });
 
   describe('#isUpdated', () => {
-    const localVersion = '2.3.6';
     const cases = [
       {
         isUpdated: false,
+        localVersion: '2.3.6',
         remoteVersion: '2.4.0',
       },
       {
         isUpdated: true,
+        localVersion: '2.3.6',
         remoteVersion: '2.3.6',
       },
       {
         isUpdated: true,
+        localVersion: '2.3.6',
+        remoteVersion: '2.3.6-0',
+      },
+      {
+        isUpdated: true,
+        localVersion: '2.3.6',
+        remoteVersion: '2.3.6-2',
+      },
+      {
+        isUpdated: true,
+        localVersion: '2.3.6-1',
+        remoteVersion: '2.3.6-2',
+      },
+      {
+        isUpdated: true,
+        localVersion: '2.3.6',
         remoteVersion: '0.3.6',
       },
       {
         isUpdated: true,
+        localVersion: '2.3.6',
         remoteVersion: '0.2.1',
       },
       {
         isUpdated: true,
+        localVersion: '2.3.6',
         remoteVersion: '2.2.1',
       },
       {
         isUpdated: true,
+        localVersion: '2.3.6',
         remoteVersion: '2.3.5',
       },
       {
         isUpdated: true,
+        localVersion: '2.3.6',
         remoteVersion: '0.2.53',
       },
       {
         isUpdated: false,
+        localVersion: '2.3.6',
         remoteVersion: '2.3.61',
       },
       {
-        isUpdated: true,
+        isUpdated: false,
+        localVersion: '2.3.6',
         remoteVersion: '2.3.59',
       },
       {
         isUpdated: false,
+        localVersion: '2.3.6',
         remoteVersion: '2.3.7',
       },
       {
         isUpdated: false,
+        localVersion: '2.3.6-0',
         remoteVersion: '4.74.452',
+      },
+      {
+        isUpdated: true,
+        localVersion: '0.10.0',
+        remoteVersion: '0.9.0',
       },
     ];
 
     cases.forEach((cas) => {
-      it(`should check the local version ${localVersion} is up to date with the remote ${cas.remoteVersion}`, (done) => {
+      it(`should check the local version ${cas.localVersion} is up to date with the remote ${cas.remoteVersion}`, (done) => {
         const mockResponse = `{"last-recomended-version": "${cas.remoteVersion}"}`;
         jest
           .spyOn(httpsService, 'get')
           .mockImplementation(() => Promise.resolve(JSON.parse(mockResponse)));
 
-        updateService.isUpdated(localVersion).then((isUpdated) => {
-          expect(isUpdated).toBe(cas.isUpdated);
-          done();
-        });
+        updateService
+          .isUpdated(cas.localVersion)
+          .then((isUpdated) => {
+            expect(isUpdated).toBe(cas.isUpdated);
+            done();
+          })
+          .catch(done);
       });
     });
   });
