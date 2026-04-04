9d31a656dd0de3048812a8bc72ea9e5376f109c6
zaldih <imzaldih@gmail.com>
2026-04-04 16:28:29 +0200

    feat(core): export logger-service interface

--- src/core/interfaces/index.ts
+++ src/core/interfaces/index.ts
@@ -4,3 +4,4 @@ export * from './services.interface.js';
 export * from './search-status.model.js';
 export * from './npkillrc-config.interface.js';
 export * from './profile.interface.js';
+export * from './logger-service.interface.js';
